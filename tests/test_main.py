c01e03cc0aa637db3ce6257fcb0ded7bfceb64e9
Дмитрий <dimnic222@yandex.com>
2025-05-27 01:07:19 +0300

    add confest.py, add fixture 12 tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,30 +3,33 @@ import src.widget
 
 
 # Маскировка карты, счета
-rs_1 = "Visa Platinum 7000792289606361"
-answer_1 = "Visa Platinum 7000 79** **** 6361"
-rs_2 = "Счет 73654108430135874305"
-answer_2 = "Счет **4305"
-zero = "0"
-empty = ""
+def test_mask_account_card(card):
+    assert src.widget.mask_account_card("Visa Platinum 7000792289606361") == card
 
 
-def test_mask_account_card():
-    assert src.widget.mask_account_card(rs_1) == answer_1
-    assert src.widget.mask_account_card(rs_2) == answer_2
-    assert src.widget.mask_account_card(zero) == zero
-    assert src.widget.mask_account_card(empty) == zero
+def test_mask_account_rs(rs):
+    assert src.widget.mask_account_card("Счет 73654108430135874305") == rs
+
+
+def test_mask_account_zero(zero):
+    assert src.widget.mask_account_card("0") == zero
+
+
+def test_mask_account_empty(zero):
+    assert src.widget.mask_account_card("") == zero
 
 
 # Преобразование дат
-date_str_1 = "2024-03-11T02:26:18.671403"
-answer_5 = "11.03.2024"
+def test_get_date(date):
+    assert src.widget.get_date("2024-03-11T02:26:18.671403") == date
 
 
-def test_get_date():
-    assert src.widget.get_date(date_str_1) == answer_5
-    assert src.widget.get_date(empty) == zero
-    assert src.widget.get_date(zero) == zero
+def test_get_date_zero(zero):
+    assert src.widget.get_date("0") == zero
+
+
+def test_get_date_empty(zero):
+    assert src.widget.get_date("") == zero
 
 
 data1 = [
@@ -35,47 +38,31 @@ data1 = [
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
-answer_8 = [
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-answer_9 = [
-    {"id": 41428820, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-]
 data1_empty_zero = [
     {"id": 41428820, "state": "", "date": ""},
     {"id": 939719570, "state": "EXECUTED", "date": "0"},
     {"id": 594226727, "state": "CANCELED", "date": "0"},
     {"id": 615064591, "state": "CANCELED_AAA", "date": ""},
 ]
-answer_10 = [
-    {"id": 939719570, "state": "EXECUTED", "date": "0"},
-]
 
 
 # Фильтр
-def test_filter_by_state():
+def test_filter_by_state_canceled(answer_8):
     assert src.processing.filter_by_state(data1, "CANCELED") == answer_8
+
+
+def test_filter_by_state_executed(answer_9):
     assert src.processing.filter_by_state(data1, "EXECUTED") == answer_9
+
+
+def test_filter_by_state_zero(answer_10):
     assert src.processing.filter_by_state(data1_empty_zero, "EXECUTED") == answer_10
 
 
-answer_11 = [
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    {"id": 41428820, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-]
-answer_12 = [
-    {"id": 41428820, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-]
-
-
 # Сортировка
-def test_sort_by_date():
+def test_sort_by_date_reverse(answer_11):
     assert src.processing.sort_by_date(data1, reverse=False) == answer_11
+
+
+def test_sort_by_date_ss(answer_12):
     assert src.processing.sort_by_date(data1) == answer_12
