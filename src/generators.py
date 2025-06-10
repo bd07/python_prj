34100732e43e64c99e38cce9c914dc0c5da67539
Дмитрий <dimnic222@yandex.com>
2025-06-10 23:40:00 +0300

    Add function card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -25,6 +25,18 @@ def transaction_descriptions(transactions):
         yield description
 
 
+def card_number_generator(start, end):
+    """
+    Генератор номеров банковских карт в формате XXXX XXXX XXXX XXXX.
+    """
+    for number in range(start, end + 1):
+        # Форматируем число с ведущими нулями до 16 цифр
+        card_number = f"{number: 016d}"
+        # Разбиваем на группы по 4 цифры
+        formatted_number = ' '.join([card_number[i:i + 4] for i in range(0, 16, 4)])
+        yield formatted_number
+
+
 transactions_1 = [
     {
         "id": 939719570,
@@ -82,3 +94,9 @@ print("")
 descriptions = transaction_descriptions(transactions_1)
 for transaction_2 in descriptions:
     print(transaction_2)
+
+print("")
+print("")
+
+for card_number_1 in card_number_generator(1, 8):
+    print(card_number_1)
